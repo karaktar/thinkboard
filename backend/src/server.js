f6cb441c9f5d2715d0ea94e85f6b0218dfa6edfc
guru <devops.guru@yahoo.com>
2025-06-24 16:48:57 -0400

    pre production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./config/config.js";
 import dotenv from "dotenv";
@@ -12,13 +13,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 
 
 // Middleware -- 
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
+
 // This function is to get access to req.body object--{title, content}, so we can use them to handle the NOTES.
 app.use(express.json());
 app.use(rateLimiter);
@@ -28,11 +34,23 @@ app.use(rateLimiter);
 // next() is the next function in the stack 
 
 app.use((req, res, next) => {
-    console.log("Middleware example", `Req method is: ${req.method} & Req URL is: ${req.url}`);
+    // console.log("Middleware example", `Req method is: ${req.method} & Req URL is: ${req.url}`);
+    console.log(`Req method is: ${req.method} & Req URL is: ${req.url}`);
     next();
 });
 
 app.use("/api/notes", notesRoutes);
+
+// If in production 
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+    });
+}
 
 // Connect to DB then start listening to requests 
 
